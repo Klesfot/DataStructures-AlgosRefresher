db929b9147051c2c30e40b8242d700f597d15c7a
Klesfot <mukhamediev.l@gmail.com>
2022-04-18 22:25:54 +0400

    Implemented a hash table w/ example search

---
+++ DataStructuresAlgosRefresher/HashTable.cs
@@ -0,0 +1,118 @@
+﻿namespace DataStructuresAlgosRefresher
+{
+    internal class HashTable
+    {
+        private const int _initialCapacity = 50;
+
+        private readonly int capacity;
+        private readonly HTNode[] buckets;
+
+        public int Size { get; private set; }
+
+        public HashTable()
+        {
+            capacity = _initialCapacity;
+            Size = 0;
+            buckets = new HTNode[capacity];
+        }
+
+        public void Add(string key, int value)
+        {
+            Size++;
+            var index = Hash(key);
+            var node = buckets[index];
+
+            if (node == null)
+            {
+                buckets[index] = new HTNode(key, value);
+                return;
+            }
+
+            // Collision
+            var prev = node;
+            while (node != null)
+            {
+                prev = node;
+                node = node.next;
+            }
+            prev.next = new HTNode(key, value);
+
+            return;
+        }
+
+        public int Remove(string key)
+        {
+            var index = Hash(key);
+            var node = this.buckets[index];
+            int result;
+
+            var prev = new HTNode();
+
+            while (node != null && node.key != key)
+            {
+                prev = node;
+                node = node.next;
+            }
+            if (node == null)
+                return -1;
+            else
+            {
+                Size--;
+                result = node.value;
+
+                if (prev == null)
+                {
+                    node = null;
+                }
+                else
+                {
+                    prev.next = null;
+                }
+                return result;
+            }
+        }
+
+        public int Find(string key)
+        {
+            var index = Hash(key);
+            var node = this.buckets[index];
+
+            while (node != null && node.key != key)
+            {
+                node = node.next;
+            }
+            if (node == null)
+                return -1;
+            else
+            {
+                return node.value;
+            }
+        }
+
+        private int Hash(string key)
+        {
+            var hashSum = 0;
+            for (var i = 0; i < key.Length; i++)
+            {
+                hashSum += (int)Math.Pow(i + key.Length, (int)key[i]);
+                hashSum %= capacity;
+            }
+
+            return hashSum;
+        }
+    }
+
+    internal class HTNode
+    {
+        public string key;
+        public int value;
+        public HTNode? next;
+
+        public HTNode(string key = "", int value = 0)
+        {
+            this.key = key;
+            this.value = value;
+            this.next = null;
+        }
+    }
+}
---
+++ DataStructuresAlgosRefresher/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace DataStructuresAlgosRefresher
+{
+    public class DataStructuresAlgorithms
+    {
+        public static void Main(string[] args)
+        {
+            // Testing the HashTable using a common problem of finding elements
+            // of one array in another
+            int[] arr1 = new int[] { 56, 7, 14, 12, 2, 9, 8 };
+            int[] arr2 = new int[] { 7, 6, 11, 8, 12, 3, 56, 7, 14, 12, 2, 9 };
+
+            var ht = new HashTable();
+
+            for (int i = 0; i < arr2.Length; i++)
+            {
+                ht.Add(Convert.ToString(arr2[i]), arr2[i]);
+            }
+
+            for (int i = 0; i < arr1.Length; i++)
+            {
+                Console.WriteLine(ht.Find(Convert.ToString(arr1[i])));
+            }
+
+            ht.Remove("56");
+            ht.Remove("7");
+
+            for (int i = 0; i < arr1.Length; i++)
+            {
+                Console.WriteLine(ht.Find(Convert.ToString(arr1[i])));
+            }
+        }
+    }
+}
